b926e1c70265afcf364f9f3da73e12f59c71a58e
saebbi1991 <101138782+saebbi1991@users.noreply.github.com>
2022-03-08 22:57:05 +0000

    e

--- assignment3/example_1.dart
+++ assignment3/example_1.dart
@@ -4,9 +4,15 @@ import 'dart:math';
 void ex1(){
   print("Ex. 1. \n Using for-loop, write a program that prints the first 10 natural numbers (1 to 10) and prints out their sum. \n The for-loop should be responsible for adding each number to it´s previous sum. Finally print the sum. \n\n Correct sum is 55.");
 
- for(int i = 1; i <= 10; i++)
-  {
-    print(i);
-  }
+ for(int i = 1; i <= 10; i++) {
+   print(i);
+ }{
+   int sum = 0;
+   for(int i = 1; i <= 10; i++) {
+     print(i);
+     sum += i; }
+   print("This is the sum of all the numbers above.");
+   print(sum); }
+
 
 }
--- assignment3/example_2.dart
+++ assignment3/example_2.dart
@@ -3,6 +3,6 @@ import 'dart:convert';
 import 'dart:math';
 void ex2(){
   print("Ex. 2. \n Write a program that accepts an input number from user and prints the multiplication (1-10) table of the given number using for loop. \n The program should be able to accept any number.");
-  
+
 
 }
